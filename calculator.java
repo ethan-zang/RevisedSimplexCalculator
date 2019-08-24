0b55331d5b9bfb9cbcadaa2ea50422580dcc784c
Ethan Zang <zang.ethan@gmail.com>
2019-08-23 22:20:15 -0400

    running matlab script through java

--- calculator.java
+++ calculator.java
@@ -1,8 +1,18 @@
+import com.mathworks.engine.MatlabEngine;
+
 public class calculator {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		int[][] a = {{2, -1, 3}, {1, 3, 5}, {2, 0, 1}};
+		int[][] b = {{6}, {10}, {7}};
+		int[][] c = {{2}, {1}, {3}};
 
+		MatlabEngine eng = MatlabEngine.startMatlab();
+
+		Object[] results = eng.feval(3,"main");
+
+		double optVal = (double)results[0];
+
+		System.out.println(optVal);
+		eng.close();
 	}
-
-	public static void compute()
 }
