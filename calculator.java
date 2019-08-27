c56887e05899c7a335c905ffbc27970d3017f4d4
“ethan-zang” <ethan.zang@rutgers.edu>
2019-08-26 21:09:24 -0400

    calling matlab with java parameters

--- calculator.java
+++ calculator.java
@@ -5,31 +5,21 @@ import com.mathworks.engine.MatlabEngine;
 
 public class calculator {
 	public static void main(String[] args) throws Exception {
-		int[][] a = {{2, -1, 3}, {1, 3, 5}, {2, 0, 1}};
-		int[][] b = {{6}, {10}, {7}};
-		int[][] c = {{2}, {1}, {3}};
+		//Temporarily defining the parameters of the problem to link to MATLAB
+		double[][] a = {{2, -1, 3}, {1, 3, 5}, {2, 0, 1}};
+		double[][] b = {{6}, {10}, {7}};
+		double[][] c = {{2}, {1}, {3}};
 
 		MatlabEngine eng = MatlabEngine.startMatlab();
 
+		//Calling MATLAB main.m function
 		Object[] results = eng.feval(3,"main",a,b,c);
 
 		double optVal = (double)results[0];
-		double[][] basis = results[1];
-		double[][] basicVars = results[2];
+		//double[][] basis = results[1];
+		//double[][] basicVars = results[2];
 
 		System.out.println(optVal);
-		
-		for (int i = 0; i < basis.length; i++)	{
-			for (int j = 0; j < basis[0].length; j++)	{
-				System.out.println(basis[i][j]);
-			}
-		}
-		
-		for (int i = 0; i < basicVars.length; i++)	{
-			for (int j = 0; j < basicVars[0].length; j++)	{
-				System.out.println(basicVars[i][j]);
-			}
-		}
 
 		eng.close();
 	}
